50252be08ee7609bd3c6ddbae6097f34a51d3b1b
Nigel Thorne <github@nigelthorne.com>
2015-04-28 18:37:43 +1000

    shopping list totals ingredients if they are the same size

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Submit the meals you could like to cook, and for how many people.
 Get a summary shopping list:
 * with quantities adjusted for head count
 * with duplicate ingredients consolidated 
+
+
+TODO
+====
+
+apply patterns from : http://blog.carbonfive.com/2013/06/24/sinatra-best-practices-part-one/
+
+* add multiple recipes
--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require 'sinatra/base'
 require 'fileutils'
 require 'sinatra/cross_origin'
  
+ require 'ostruct'
 require 'open-uri'
 # Application:::::::::::::::::::::::::::::::::::::::::::::::::::
 require_relative "sass_handler"
@@ -30,26 +31,73 @@ class MyApp < Sinatra::Base
   end
 
   # Configuration:::::::::::::::::::::::::::::::::::::::::::::::
-  set :public, File.dirname(__FILE__) + '/public'
+  set :public_dir, File.dirname(__FILE__) + '/public'
   set :views, File.dirname(__FILE__) + '/views'
   set :protection, false
+  enable :sessions
   # Route Handlers::::::::::::::::::::::::::::::::::::::::::::::
 
-  get '/' do
-    @author = "Nigel Thorne"
-    @year = Time.now.year
-    slim :index
+  def require_logged_in
+    redirect('/sessions/new') unless is_authenticated?
   end
+   
+  def is_authenticated?
+    return !!session[:user_id]
+  end
+   
+  get '/' do
+    slim :login
+  end
+   
+  get '/sessions/new' do
+    slim :login
+  end
+   
+  post '/sessions' do
+    session[:user_id] = params["user_id"]
+    redirect('/meal_plan')
+  end
+   
+  get '/meal_plan' do
+    require_logged_in
+    slim :meal_plan
+  end
+
+  post '/shopping_list' do
+    urls = params["recipe_url"].values.reject{|v| v.nil? || v.empty? }
+    @verbose_ingredients = urls.flat_map{|u| ingredients(u) }
+    @grouped_ingredients = @verbose_ingredients.group_by{|i| [i.ingredient, i.unit]}
+    @ingredients = @grouped_ingredients.map{|k,v| OpenStruct.new({ingredient: k[0], unit:k[1], amount:v.inject(0){|a,t| a+t.amount}})}
+    slim :shopping_list
+  end
+
+  # TODO.. take into account head count
+  def ingredients(url)
+    recipe_html_string = open(url).read
+
+    recipe = Hangry.parse(recipe_html_string)
+    ingredients = recipe.ingredients.map{
+      |ingredient|
+      Ingreedy.parse(ingredient)
+    }
+  end
+  # get '/' do
+  #   @author = "Nigel Thorne"
+  #   @year = Time.now.year
+  #   slim :index
+  # end
 
   post '/recipe' do
     recipe_html_string = open(params["recipe_url"]).read
 
     @recipe = Hangry.parse(recipe_html_string)
-    @ingredients = @recipe.ingredients.map{
+    @verbose_ingredients = @recipe.ingredients.flat_map{
       |ingredient|
       Ingreedy.parse(ingredient)
     }
-
+    @grouped_ingredients = @verbose_ingredients.group_by{|i| [i.ingredient, i.unit]}
+    @ingredients = @grouped_ingredients.map{|k,v| v[0].amount = v.inject{|a,t| t+a.amount}}
+    raise @ingredients.inspect
     slim :recipe
   end
         
